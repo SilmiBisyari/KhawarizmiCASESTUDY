84a2ca143bcb6203df36ecd51f8b66940b1b0b7b
SilmiBisyari <silmibisyari7@gmail.com>
2020-11-30 13:39:43 +0800

    changes

--- poscasestudy/test/Discount.dart
+++ poscasestudy/test/Discount.dart
@@ -1,11 +1,27 @@
 import 'dart:core';
+import 'dart:io';
 class Discount {
 
+  double quantity;
+  double gst;
+  double NAmount;
   int discount;
   String number;
   double number2;
   double after;
-  double fine;
+
+
+  double total;
+
+  var foodlist1 = ['Ayam','Fish','Duck'];
+  var productDescription = ['Animal foods','Animal foods','Animal foods'];
+  var priceproduct = ['8.34','6.83','7.90'];
+  var priceproduct2 = [8.34,6.83,7.90];
+  List <String> eachquantity = new List();
+  List <double> eachproductquantity = new List();
+  List <double> eachproductquantityandprices =new List();
+  List <double> totaleachproductpricesandquantity = new List();
+
 
   void setdiscount(String number) {
     this.number = number;
@@ -16,16 +32,56 @@ class Discount {
   void afterdiscount(){
 
     double fine = 0;
-    double number2 = double.parse(number);
-    fine = number2 - (number2 * 36/100);
+    fine = total - (total * 36/100);
     this.after = fine;
   }
-
-  void calculationdiscount(newnumber){
-    newnumber.getnumber();
-    newnumber.afterdiscount();
-    print("calculation : RM$after" );
-
+  void printinventory(){
+    for(int i = 0;i<foodlist1.length;i++){
+      print("  " +foodlist1[i] + "      " + "  " + productDescription[i] + "  " +"      RM" + priceproduct[i] + "  " +"      " + eachquantity[i] + "  " +"      RM" + totaleachproductpricesandquantity[i].toString() );
+    }
   }
+  void askQuantity(){
+    eachquantity.add(getnumber());
+  }
+  void changesquantity() {
+    for (int M = 0; M < foodlist1.length; M++) {
+      double temp = double.parse(eachquantity[M]);
+      eachproductquantity.add(temp);
+    }
+    calculateeachproductprice();
+  }
+  void calculateeachproductprice(){
+  double sum ;
+    for (int a = 0;a < priceproduct.length; a++) {
+      sum = eachproductquantity[a] * priceproduct2[a];
+      totaleachproductpricesandquantity.add(sum);
+    }
+  }
+  void totalprices() {
+    double total = 0;
+    for (int k = 0; k < foodlist1.length; k++) {
+      total = total + totaleachproductpricesandquantity[k];
+    }
+    this.total = total ;
+  }
+  void calculationtax(){
+    double gst = 0.06;
+    NAmount = after + (after * gst);
+    this.NAmount = NAmount;
+  }
+  void calculationdiscount(){
+    afterdiscount();
+    print("  calculation after discount :RM$after" );
+  }
+  void displayresult(){
+    totalprices();
+    calculationdiscount();
+    calculationtax();
+    printinventory();
+    print("                                                   total: RM$total");
+    print("                                    total after discount: RM$after");
+    print("                                    Total after tax     : RM$NAmount");
+  }
+
 }
 
--- poscasestudy/test/main.dart
+++ poscasestudy/test/main.dart
@@ -1,23 +1,38 @@
 import 'dart:io';
+import 'dart:core';
 import 'Discount.dart';
+
 void main(){
-  DateTime today = new DateTime.now();
-  var newlist = new List();
+
+  DateTime dateTime = DateTime.now();
+  var newnumber = new Discount();
+
+  //print date
+  String currdate = dateTime.toIso8601String().split('T').first;
+
+
   var title = """ 
-  WELCOME TO THE POS SUPERMARKET
-  ==============================
-  $today
+  WELCOME TO THE POS SUPERMARKET   ($currdate)
+  ============================================
+  ============================================  
   """;
   print(title);
 
-  stdout.write("  Number: RM");
-  var newnumber = new Discount();
 
-  newnumber.setdiscount(stdin.readLineSync());
-  newnumber.calculationdiscount(newnumber);
+  for(int i = 0 ; i<newnumber.foodlist1.length;i++) {
+    print(newnumber.foodlist1[i]);
+    print("how much the quantity : ");
+    newnumber.setdiscount(stdin.readLineSync());
+    newnumber.askQuantity();
+  }
+  //newnumber.calculationtax();
+  print("  =============================================================================");
+  print("                                  Receipt                                      ");
+  print("  Items      Description      Price(each)   Quantity   Total(prices X Quantity) ");
 
-  newlist.add(newnumber.getnumber);
-  print(newlist);
+  //print("Did you want your receipt? (Y/N)");
+  newnumber.changesquantity();
 
-  print(today);
+  newnumber.displayresult();
 }
+
