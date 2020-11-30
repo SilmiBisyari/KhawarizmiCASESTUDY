852e55f0dc2c29d6597cd272a01c17a4e22afc76
FarisAsh97 <72858166+FarisAsh97@users.noreply.github.com>
2020-11-30 13:59:53 +0800

    Add files via upload

---
+++ poscasestudy/test/gst.dart
@@ -0,0 +1,28 @@
+// Muhd Faris Asyraaf
+import 'dart:core';
+import 'dart:io';
+class gst {
+
+  double gst;
+
+  var TAmount;
+  var NAmount;
+
+  gst = TAmount * 0.06;
+  NAmount = TAmount + gst;
+  this.NAmount;
+  return ("Total Amount : $NAmount");
+}
+
+class GST
+{
+  static float Calculate_GST(float org_cost,
+      float N_price)
+  {
+    // return value after calculate GST%
+    gst = TAmount * 0.06;
+    NAmount = TAmount + gst;
+    this.NAmount;
+    return ("Total Amount : $NAmount");
+  }
+}
